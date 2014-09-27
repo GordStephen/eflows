12413a02764a999a00ce9c078fda558fa4a801ab
Gord Stephen <gord@gordstephen.ca>
2014-09-27 16:29:20 -0400

    Added data models

--- eflows.py
+++ eflows.py
@@ -1,5 +1,6 @@
 import argparse
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine, Table, Column, Integer, String, ForeignKey
+from sqlalchemy.schema import ForeignKeyConstraint 
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -22,7 +23,65 @@ class Resource(Base):
     name = Column(String)
 
     def __repr__(self):
-        return "<Resource(name='%s')>" % (self.name)
+        return "<Resource '%s'>" % (self.name)
+
+class NodeCategoryClass(Base):
+    __tablename__ = 'node_category_classes'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    #categories = relationship(NodeCategory, backref='class')
+
+    def __repr__(self):
+        return "<NodeCategoryClass '%s'>" % (self.name)
+
+class NodeCategory(Base):
+    __tablename__ = 'node_categories'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    class_id = Column(Integer, ForeignKey('node_category_classes.id'))
+    #nodes = relationship(Node
+
+    def __repr__(self):
+        return "<NodeCategory '%s'>" % (self.name)
+
+class Node(Base):
+    __tablename__ = 'nodes'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    category_id = Column(Integer, ForeignKey('node_categories.id'))
+
+resource_source_nodes = Table(
+    'resource_source_nodes', Base.metadata,
+    Column('resource_id', Integer, ForeignKey('resources.id')),
+    Column('node_id', Integer, ForeignKey('nodes.id'))
+)
+
+resource_sink_nodes = Table(
+    'resource_sink_nodes', Base.metadata,
+    Column('resource_id', Integer, ForeignKey('resources.id')),
+    Column('node_id', Integer, ForeignKey('nodes.id'))
+)
+
+class Flow(Base):
+    __tablename__ = 'flows'
+
+    id = Column(Integer, primary_key=True)
+    resource_id = Column(Integer, ForeignKey('resources.id'))
+    source_node_id = Column(Integer) 
+    sink_node_id = Column(Integer) 
+
+    ForeignKeyConstraint(
+        ['resource_id', 'source_node_id'], 
+        ['resource_source_nodes.resource_id','resource_source_nodes.node_id']
+    )
+
+    ForeignKeyConstraint(
+        ['resource_id', 'sink_node_id'], 
+        ['resource_sink_nodes.resource_id','resource_sink_nodes.node_id']
+    )
 
 if args.load_data:
     Base.metadata.create_all(engine)
